eca6cd509951bf69727b4ee84a4f4a665019d451
rubenverkuylen <info@rubenverkuylen.nl>
2025-04-29 18:40:27 +0200

    scrolling update

--- script.js
+++ script.js
@@ -15,8 +15,10 @@ function scrollFunction() {
 }
 function scrollToTop() {
   buttonTop.classList.remove("show");
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
 
 buttonTop.addEventListener("click", scrollToTop);
